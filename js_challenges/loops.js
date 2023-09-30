3808ab0c2872c0d98a97fb1dd5f34afdef22f247
= <=ilyasmahamed7@gmail.com>
2023-09-30 12:54:31 +0100

    first commit

---
+++ js_challenges/loops.js
@@ -0,0 +1,42 @@
+for(let i=0;  i<10; i++ ) {
+    console.log("Ilyas the goat ")
+}
+
+
+let i = 0 
+while(i < 10 ) {
+   i++ 
+   console.log("Ilyas/Mahamed")
+}
+    
+
+
+
+
+let today = "Saturday"
+if (today=="Saturday") {
+    console.log("Come to the coding school")
+} else if(today == "Sunday") {
+    console.log("Sleep")
+   }   else if (today == "Monday") {
+    console.log("go to school")
+   } else {
+    console.log("Do whatever you like")
+   }
+
+    switch (today) {
+
+        case "Saturday" :
+            console.log("Coding time...")
+            break;
+            case "Sunday" :
+            console.log("Sleeping time...")
+            break;
+            case "Monday" :
+            console.log("School time...")
+            break;
+            
+            
+    }
+
+
